ab11a62cf083d355d37e27ab83e400ef0f51c264
Alex Redmon <alex.redmon@cylera.com>
2019-12-21 16:55:49 -0500

    Create React App eject & config

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { StyleSheet, Text, View } from 'react-native';
 
-function App() {
+import AppContainer from 'common/containers/AppContainer'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        Wall of Ads!
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer />
+  )
 }
 
-export default App;
+export default App
+
+
+
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     backgroundColor: '#fff',
+// //     alignItems: 'center',
+// //     justifyContent: 'center',
+// //   },
+// // });
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         Wall of Ads!
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
