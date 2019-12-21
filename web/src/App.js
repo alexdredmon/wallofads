13280fa887e8afa29b5ba6e4a49c6945b8ade5ce
Alex Redmon <alex.redmon@cylera.com>
2019-12-21 18:13:30 -0500

    Revert "Create React App eject & config"
    This reverts commit ab11a62cf083d355d37e27ab83e400ef0f51c264.

--- web/src/App.js
+++ web/src/App.js
@@ -1,51 +1,26 @@
 import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import logo from './logo.svg';
+import './App.css';
 
-import AppContainer from 'common/containers/AppContainer'
-
-const App = () => {
+function App() {
   return (
-    <AppContainer />
-  )
+    <div className="App">
+      <header className="App-header">
+        Wall of Ads!
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
-
-
-
-// // const styles = StyleSheet.create({
-// //   container: {
-// //     flex: 1,
-// //     backgroundColor: '#fff',
-// //     alignItems: 'center',
-// //     justifyContent: 'center',
-// //   },
-// // });
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         Wall of Ads!
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
